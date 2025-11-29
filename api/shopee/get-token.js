a57d88fb0629e19370cdffa636cc1223495ee22a
cleitonschenckel <cleitonschenckel@gmail.com>
2025-11-29 15:40:45 -0300

    Create get-token.js

---
+++ api/shopee/get-token.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  const partnerId = process.env.PARTNER_ID;
+  const redirectUrl = process.env.REDIRECT_URL;
+
+  const authURL =
+    `https://partner.shopeemobile.com/api/v2/authenticate?` +
+    `partner_id=${partnerId}` +
+    `&redirect=${encodeURIComponent(redirectUrl)}`;
+
+  return res.status(200).json({ url: authURL });
+}
