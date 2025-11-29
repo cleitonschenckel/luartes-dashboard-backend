e0994ad02f4b44b8c231d8bddd898cef7c9f0de0
cleitonschenckel <cleitonschenckel@gmail.com>
2025-11-29 15:41:41 -0300

    Add callback handler for Shopee API

---
+++ api/shopee/callback.js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+  const { code } = req.query;
+
+  if (!code) {
+    return res.status(400).json({
+      error: "Código não recebido",
+    });
+  }
+
+  return res.status(200).json({
+    message: "Código recebido com sucesso",
+    code,
+  });
+}
